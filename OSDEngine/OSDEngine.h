204f5fd2970b2b985c1842dfebcfb2ce05a9b00c
Skylar Schipper <ss@schipp.co>
2014-11-24 20:23:38 -0800

    Initial Commit

---
+++ OSDEngine/OSDEngine.h
@@ -0,0 +1,19 @@
+//
+//  OSDEngine.h
+//  OSDEngine
+//
+//  Created by Skylar Schipper on 11/24/14.
+//  Copyright (c) 2014 OpenSky, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OSDEngine.
+FOUNDATION_EXPORT double OSDEngineVersionNumber;
+
+//! Project version string for OSDEngine.
+FOUNDATION_EXPORT const unsigned char OSDEngineVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OSDEngine/PublicHeader.h>
+
+
